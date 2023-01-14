2c710545ddf6a2405aea073d8dbced694ec71c96
MOAZZEM HOSEN SARDAR <92745004+moazzemhosen@users.noreply.github.com>
2023-01-14 18:50:13 +0530

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React from "react"
 import './App.css';
 import MainRoutes from './Routes/MainRoutes';
 
--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React,{Suspense,lazy} from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Table from '../pages/Table'
+//import Table from '../pages/Table'
 import Details from '../pages/Details'
-
+const Table=lazy(()=>import('../pages/Table'))
 const MainRoutes = () => {
   return (
     <div>
+      <Suspense fallback={<div>Loading..</div>}>
         <Routes>
             <Route path='/' element={<Table/>}/>
             <Route path='/:id' element={<Details/>}/>
         </Routes>
+        </Suspense>
     </div>
   )
 }
